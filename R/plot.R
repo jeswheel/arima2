558a50eee6f9493b2f0b679c5cbb3c019e73eeab
Jesse Wheeler <jeswheel@umich.edu>
2022-12-03 09:18:02 -0500

    Fixed warning with plot function

--- R/plot.R
+++ R/plot.R
@@ -4,13 +4,16 @@
 #'
 #' TODO: This could plot data, data + sims, or roots, roots + data, roots + data + sims.
 #'
-#' @param object An `Arima2` object.
+#' @param x An `Arima2` object.
 #' @param ... All inputs to the plot function
 #' @return NULL
 #' @export
 #'
 #' @examples
 #' mod <- plot(arima2(lh, order = c(1,0,1)))
-plot.Arima2 <- function(object, ...) {
-  plot(object$x, type = 'l', ...)
+plot.Arima2 <- function(x, ...) {
+  # x is an ARIMA2 object, x$x contains the data.
+  xaxis <- 1:length(x$x)
+  yaxis <- x$x
+  plot(xaxis, yaxis, type = 'l', ...)
 }
