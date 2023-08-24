8c22c391b4304079c5440bea21f6a7ffd82600f5
Jesse Wheeler <jeswheel@umich.edu>
2023-08-24 14:06:00 -0400

    Added Lake Michigan-Huron January levels to package

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Annual January levels of lake Michigan-Huron
+#'
+#' The dataset is a subset of the monthly average depth (ft) of lake
+#' Michigan-Huron. The data were retrieved online from  the
+#' Great Lakes Environmental Research Laboratory. Various measurement gauges
+#' exist; this data was taken from the master gauge.
+#'
+#' @format ## `miHuron_level`
+#' A data frame with 155 observations and two columns:
+#' \describe{
+#'   \item{Date}{`Date` column that records when the observation was made.}
+#'   \item{Average}{`numeric` column representing the average depth in feet.}
+#' }
+#' @source <https://www.glerl.noaa.gov/data/dashboard/data/levels/mGauge/miHuronMog.csv>
+"miHuron_level"
